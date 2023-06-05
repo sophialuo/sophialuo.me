cbe75579633403a146a14fd76637cc28c4a7022a
Sophia Luo <sophia.luo@scale.com>
2023-06-04 19:21:42 -0700

    CSS of basic layout

--- src/components/UltimateTicTacToe.tsx
+++ src/components/UltimateTicTacToe.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import _ from "lodash";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./UltimateTicTacToe.css";
+
+const N = 3;
 
 const UltimateTicTacToe: React.FC = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <div onClick={() => navigate(-1)}>Back</div>
-      <div>ultimate tic tac toe</div>
+    <div className="container">
+      <div className="header">
+        <Link to="/">Back</Link>
+      </div>
+      <div className="game-wrapper">
+        <h1>Ultimate Tic Tac Toe</h1>
+        <div className="board">
+          {_.range(N).map((row: number) => {
+            return (
+              <div className="board-row">
+                {_.range(N).map((col: number) => {
+                  return (
+                    <div
+                      key={`ultimate-tic-tac-toe_${row}_${col}`}
+                      className={`tile tile_${row === N - 1 ? row : "r"}_${
+                        row === N - 1 && col !== N - 1 ? "c" : col
+                      }`}
+                    >
+                      Coming soon!
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
