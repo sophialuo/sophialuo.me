a8fff235f4b8dcc40a2a49efbb099af888d43038
Sophia Luo <luo.sophia.y@gmail.com>
2023-06-19 13:35:50 -0700

    refactor home page and body row components

--- app/components/Home/BodyRow.tsx
+++ app/components/Home/BodyRow.tsx
@@ -1,52 +1,17 @@
-"use client";
-
 import React from "react";
-import { useRouter } from "next/navigation";
 import _ from "lodash";
 import "./BodyRow.css";
 
-const NUM_ITEMS = 10;
-
 interface BodyRowProps {
   rowTitle: string;
+  rowItems: React.JSX.Element[];
 }
 
-// TODO: insert image to display
-const tinkeringItems = [
-  {
-    path: "/ultimate-tic-tac-toe",
-    name: "Ultimate Tic Tac Toe",
-  },
-];
-
-const BodyRow: React.FC<BodyRowProps> = ({ rowTitle }) => {
-  const router = useRouter();
-
+const BodyRow: React.FC<BodyRowProps> = ({ rowTitle, rowItems }) => {
   return (
     <div>
       <div className="header-title">{rowTitle}</div>
-      <div className="items">
-        {rowTitle === `TINKERING` &&
-          tinkeringItems.map((item) => {
-            const { path, name } = item;
-            return (
-              <div
-                key={`${rowTitle}_${path}`}
-                className="coming-soon-block"
-                onClick={() => router.push(path)}
-              >
-                {name}
-              </div>
-            );
-          })}
-        {_.range(NUM_ITEMS).map((index: number) => {
-          return (
-            <div key={`${rowTitle}_${index}`} className="coming-soon-block">
-              Coming soon!
-            </div>
-          );
-        })}
-      </div>
+      <div className="items">{rowItems.map((item) => item)}</div>
     </div>
   );
 };
--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -1,16 +1,73 @@
+"use client";
+
+import _ from "lodash";
 import React from "react";
 import IntroRow from "./IntroRow";
 import BodyRow from "./BodyRow";
+import { useRouter } from "next/navigation";
+
 import "./styles.css";
+
+const NUM_ITEMS = 10;
+
+const tinkeringItems = [
+  {
+    path: "/ultimate-tic-tac-toe",
+    name: "Ultimate Tic Tac Toe",
+  },
+];
+
+const RowItemPreview: React.FC<{
+  rowTitle: string;
+  index: number;
+  itemProps?: { path: string; name: string };
+}> = ({ rowTitle, index, itemProps }) => {
+  const router = useRouter();
+
+  return (
+    <div
+      key={`${rowTitle}${index}`}
+      className="coming-soon-block"
+      onClick={() => {
+        if (itemProps?.path) {
+          router.push(itemProps.path);
+        }
+      }}
+    >
+      {itemProps?.name ?? "Coming Soon"}
+    </div>
+  );
+};
 
 const Home: React.FC = () => {
   return (
     <div className="container">
       <IntroRow />
       <div className="row-wrapper">
-        <BodyRow rowTitle="TINKERING" />
-        <BodyRow rowTitle="EXPRESSING" />
-        <BodyRow rowTitle="THINKING" />
+        <BodyRow
+          rowTitle="TINKERING"
+          rowItems={_.range(NUM_ITEMS).map((index: number) => {
+            return (
+              <RowItemPreview
+                rowTitle={"TINKERING"}
+                index={index}
+                itemProps={tinkeringItems[index]}
+              />
+            );
+          })}
+        />
+        <BodyRow
+          rowTitle="EXPRESSING"
+          rowItems={_.range(NUM_ITEMS).map((index: number) => {
+            return <RowItemPreview rowTitle={"EXPRESSING"} index={index} />;
+          })}
+        />
+        <BodyRow
+          rowTitle="THINKING"
+          rowItems={_.range(NUM_ITEMS).map((index: number) => {
+            return <RowItemPreview rowTitle={"THINKING"} index={index} />;
+          })}
+        />
       </div>
     </div>
   );
