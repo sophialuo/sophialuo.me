0f13d6ef9f99f759b901c9fe5a0ac60446a2d678
Sophia Luo <luo.sophia.y@gmail.com>
2023-06-24 17:35:21 -0700

    Bubble preview

--- components/BouncyBubbles/Bubble.tsx
+++ components/BouncyBubbles/Bubble.tsx
@@ -4,37 +4,74 @@ import _ from "lodash";
 import React from "react";
 import "./styles.css";
 
-interface BubbleProps {
+export interface BubbleProps {
   top: number;
   left: number;
   width: number;
   height: number;
+  position?: "static" | "relative" | "absolute" | "sticky" | "fixed";
+  bubbleDelay?: number;
+  animationKeyframe?: string;
+  animationTime?: number;
 }
 
 const BUFFER = 4;
 
-const getAnimationVars = ({ top, height }: { top: number; height: number }) => {
-  if (top - BUFFER < height) {
+const getAnimationVars = ({
+  top,
+  height,
+  bubbleDelay,
+  animationKeyframe,
+  animationTime,
+}: {
+  top: number;
+  height: number;
+  bubbleDelay?: number;
+  animationKeyframe?: string;
+  animationTime?: number;
+}) => {
+  if (top >= 0 && top - BUFFER < height) {
     return {
-      "--bounceHeight": `-${top - BUFFER}px`,
+      "--bounceHeight": `-${Math.abs(top - BUFFER)}px`,
+      "--bubbleDelay": `${bubbleDelay ?? 0}s`,
+      "--animationKeyframe": `${animationKeyframe ?? "float"}`,
+      "--animationTime": `${animationTime ?? 6}s`,
     };
   } else {
     return {
       "--bounceHeight": `-${height}px`,
+      "--bubbleDelay": `${bubbleDelay ?? 0}s`,
+      "--animationKeyframe": `${animationKeyframe ?? "float"}`,
+      "--animationTime": `${animationTime ?? 6}s`,
     };
   }
 };
-const Bubble: React.FC<BubbleProps> = ({ top, left, width, height }) => {
+const Bubble: React.FC<BubbleProps> = ({
+  top,
+  left,
+  width,
+  height,
+  position,
+  bubbleDelay,
+  animationKeyframe,
+  animationTime,
+}) => {
   return (
     <div
       className="secondary bubble"
       style={{
-        position: "absolute",
+        position: position ?? "absolute",
         top,
         left,
         width,
         height,
-        ...getAnimationVars({ top, height }),
+        ...getAnimationVars({
+          top,
+          height,
+          bubbleDelay,
+          animationKeyframe,
+          animationTime,
+        }),
       }}
     ></div>
   );
--- components/Home/Preview/BouncyBubbles/index.tsx
+++ components/Home/Preview/BouncyBubbles/index.tsx
@@ -1,15 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import _ from "lodash";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Bubble, { BubbleProps } from "../../../BouncyBubbles/Bubble";
+import "../../../BouncyBubbles/styles.css";
+import "./styles.css";
 
 const PATH = "/bouncy-bubbles";
 
+const BUBBLE_PROPS: BubbleProps[] = [
+  {
+    position: "relative",
+    top: -60,
+    left: 10,
+    width: 50,
+    height: 50,
+    bubbleDelay: 0.15,
+  },
+  {
+    position: "relative",
+    top: -90,
+    left: 70,
+    width: 20,
+    height: 20,
+  },
+  {
+    position: "relative",
+    top: -135,
+    left: 80,
+    width: 30,
+    height: 30,
+    bubbleDelay: 0.25,
+  },
+  {
+    position: "relative",
+    top: -130,
+    left: 120,
+    width: 10,
+    height: 10,
+    bubbleDelay: 0.5,
+  },
+  {
+    position: "relative",
+    top: -215,
+    left: 130,
+    width: 25,
+    height: 25,
+    bubbleDelay: 0.25,
+  },
+  {
+    position: "relative",
+    top: -180,
+    left: 150,
+    width: 15,
+    height: 15,
+  },
+  {
+    position: "relative",
+    top: -225,
+    left: 170,
+    width: 50,
+    height: 50,
+    bubbleDelay: 0.45,
+  },
+  {
+    position: "relative",
+    top: -255,
+    left: 210,
+    width: 30,
+    height: 30,
+    bubbleDelay: 0.7,
+  },
+];
+
 const BouncyBubbles: React.FC = () => {
   const router = useRouter();
+  const [hover, setHover] = useState<boolean>(false);
 
   return (
-    <div className="preview-container">
+    <div
+      className="preview-container"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <Link
         href={PATH}
         onClick={() => {
@@ -17,13 +90,25 @@ const BouncyBubbles: React.FC = () => {
         }}
       >
         <div
-          className="coming-soon-block"
+          className="primary bubbles-container"
           onClick={() => {
             router.push(PATH);
           }}
         >
-          BOUNCY BUBBLES
+          Bouncy Bubbles
         </div>
+        {BUBBLE_PROPS.map((prop) => (
+          <Bubble
+            key={`preview-${JSON.stringify(prop)}`}
+            top={prop.top}
+            left={prop.left}
+            width={prop.width}
+            height={prop.height}
+            position={prop.position}
+            animationKeyframe={!hover ? "" : undefined}
+            animationTime={3}
+          />
+        ))}
       </Link>
     </div>
   );
