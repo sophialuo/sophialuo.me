40a76cef520a990b690f4d8f8e11147d08e9f7a9
Sophia Luo <luo.sophia.y@gmail.com>
2023-06-26 12:13:16 -0700

    fix to build

--- app/art-gallery/page.tsx
+++ app/art-gallery/page.tsx
@@ -5,6 +5,11 @@ import { ArtGallery } from "../../components/ArtGallery";
 import Layout from "../../components/Layout";
 
 const Page: React.FC = () => {
+  const isBrowser = typeof window !== "undefined";
+
+  if (!isBrowser) {
+    return null;
+  }
   return (
     <Layout>
       <ArtGallery />
--- app/bouncy-bubbles/page.tsx
+++ app/bouncy-bubbles/page.tsx
@@ -5,6 +5,11 @@ import BouncyBubbles from "../../components/BouncyBubbles";
 import Layout from "../../components/Layout";
 
 const Page: React.FC = () => {
+  const isBrowser = typeof window !== "undefined";
+
+  if (!isBrowser) {
+    return null;
+  }
   return (
     <Layout>
       <BouncyBubbles />
